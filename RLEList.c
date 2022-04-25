232d447996b3b97494f1e14c8de4e498d13121af
hssainazaqy <hssain.azaqy@campus.technion.ac.il>
2022-04-25 22:16:24 +0300

    update 1 - RLEList files

--- RLEList.c
+++ RLEList.c
@@ -1,7 +1,50 @@
 #include "RLEList.h"
 
-struct RLEList_t{
-    //TODO: implement
-};
+typedef struct RLEList_t{
+    char letter;
+    struct *RLEList_t next;
+}*RLEList;
 
-//implement the functions here
+//------------------------------------------------------
+//--------------Implementations-------------------------
+//------------------------------------------------------
+RLEList RLEListCreate()
+{	
+    RLEList list = (int*)malloc(sizeof(*list));
+    if(!list)
+    {
+	return NULL;
+    }
+    list->letter = '0';
+    list->next = NULL;
+    return list;	
+}
+//------------------------------------------------------
+void RLEListDestroy(RLEList list){
+    while(list){
+        RLEList toDelete = list;
+        list = list->next;
+        free(toDelete);
+    }
+}
+//------------------------------------------------------
+RLEListResult RLEListAppend(RLEList list, char value)
+{
+    if(!list){return RLE_LIST_NULL_ARGUMENT;}
+
+    RLEList newList = RLEListCreate();
+    RLEList lastList = List;
+    
+    if(!newlist)
+    {
+        return RLE_LIST_OUT_OF_MEMORY;
+    }
+    newList->Letter = value;
+    while (!(lastList->next))
+    {
+	lastList = lastList->next;
+    }
+    lastList->next = newList;
+
+    return RLE_LIST_SUCCESS;
+}
